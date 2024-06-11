1533a4b656ea68b3d87850277efc0dfbc05e6367
Brenainn <brenainnvanhaverbeke@gmail.com>
2024-06-11 21:00:53 +0200

    Added operator overloads for convenience.

--- Minigin/Transform.cpp
+++ Minigin/Transform.cpp
@@ -7,3 +7,18 @@ void dae::Transform::SetPosition(const float x, const float y, const float z)
 	m_position.y = y;
 	m_position.z = z;
 }
+
+dae::Transform dae::Transform::operator+(const Transform& other)
+{
+	Transform newTransform{ *this };
+	newTransform += other;
+	return newTransform;
+}
+
+dae::Transform& dae::Transform::operator+=(const Transform& other)
+{
+	m_position.x += other.GetPosition().x;
+	m_position.y += other.GetPosition().y;
+	m_position.z += other.GetPosition().z;
+	return *this;
+}
--- Minigin/Transform.h
+++ Minigin/Transform.h
@@ -8,6 +8,9 @@ namespace dae
 	public:
 		const glm::vec3& GetPosition() const { return m_position; }
 		void SetPosition(float x, float y, float z);
+
+		Transform operator+(const Transform& other);
+		Transform& operator+=(const Transform& other);
 	private:
 		glm::vec3 m_position;
 	};
